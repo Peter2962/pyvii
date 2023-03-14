1b5f4545deb805ca6006d791a183001f10af7b70
Peter Damilare <mayokundamilare@gmail.com>
2023-03-14 20:06:18 +0200

    Added email validation logic

--- pyvii/rules/email.py
+++ pyvii/rules/email.py
@@ -1,7 +1,16 @@
-class Email():
-	def __init__(self):
-		self.message = '[attr] is not a valid email'
-		pass
+import re
+from ..base_rule import BaseRule
 
-	def validate():
-		pass
+class Email(BaseRule):
+	def __init__(self, attr, rule):
+		self.message = '[attr] is not a valid email'
+		super().__init__(self.message, {
+			'[attr]': attr
+		})
+
+	def validate(self, value, additional_args = ''):
+		pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+		if re.match(pattern, value):
+			return True
+
+		return False
